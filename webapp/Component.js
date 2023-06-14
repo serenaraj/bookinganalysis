348a0bc473fe4c17b31e915941419d8edf5fc83f
serenaraj <serena.raj@rizing.com>
2023-06-14 20:34:02 +0800

    initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("sap.fe.demo.sr02.bookinganalysis.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
